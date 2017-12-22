bb2a1fe4c723514ba00e0e3d5bc162b992a57c56
Pawel Kowalski <flukson@gmail.com>
2017-12-22 13:54:50 +0100

    Add support for many root files in GOJA analyzer
    When GATE simulations are performed using the cluster infrastructure,
    for each separated process, single *.root file is created.
    All these files may be analyzed at once using --root-many option
    added in this commit.

--- examples/goja/README.md
+++ examples/goja/README.md
@@ -6,8 +6,8 @@ from them.
 
 Dependencies
 ------------
-ROOT
-Boost
+ROOT v. 5.X
+Boost v. 1.50.X
 
 Building
 --------
@@ -20,5 +20,10 @@ sudo make install
 Usage
 -----
 Example:
-./goja --eth 0.2 --eth0 0.01 --tw 3 --root output.root > coincidences.txt
-Type ./goja --help for details
+goja --eth 0.2 --eth0 0.01 --tw 3 --root output.root > coincidences.txt
+If the user won't redirect the output to a file, it will be written into the
+standard output (console).
+
+Help
+----
+Type goja --help (or just goja without arguments) for details.
--- examples/goja/goja.cpp
+++ examples/goja/goja.cpp
@@ -15,7 +15,7 @@ namespace po = boost::program_options;
 
 int main (int argc, char* argv[]) {
 
-	po::options_description desc("\nGOJA help\n\nAllowed options");
+	po::options_description desc("\nGOJA (GATE Output J-PET Analyzer) help\n\nAllowed options");
 
 	desc.add_options()
     ("help", "produce help message")
@@ -25,7 +25,16 @@ int main (int argc, char* argv[]) {
 	("N", po::value<double>(), "maximum number of events above the fixed energy threshold in the coincidence window (default: 2)")
 	("N0", po::value<double>(), "maximum number of events above the noise energy threshold in the coincidence window (default: 3)")
 	("sep", po::value<int>(), "separate events using time window (arg=0) or using IDs of hits (arg=1) (default: 0)")
-    ("root", po::value<string>(), "file path of the GATE *.root file");
+	("root", po::value<string>(), "file path of the single GATE *.root file,"
+	                              "for example --root=output.root")
+	("root-many", po::value<string>(), "file path of the base GATE *.root file "
+	                                   "without number and extension) and number "
+	                                   "of files separated using comma, "
+	                                   "for example, option --root-many=output/output,100 will cause "
+	                                   "the analysis of 100 files with names from output/output1.root "
+	                                   "to output/output100.root "
+	                                   "(base name is specified in the GATE macro using the following command: "
+	                                   "/gate/output/root/setFileName output/output)");
 
 	po::variables_map vm;
 	po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -90,19 +99,35 @@ int main (int argc, char* argv[]) {
 		setenv("GOJA_SEP", "0", 1);
 	}
 
-	if (vm.count("root")) {
-		stringstream ss;
-		ss << vm["root"].as<string>();
-		cout << ss.str().c_str();
-		setenv("GOJA_ROOT_FILENAME", ss.str().c_str(), 1);
-	}
-	else {
-		cout << "You need to add root file using --root option. See help.\n";
+	if (!(vm.count("root")) and !(vm.count("root-many"))) {
+		cout << "You need to add root file(s) using --root or --root-many. See help.\n";
 		return 1;
 	}
+	else if (vm.count("root")) {
+		stringstream ss;
+		ss << vm["root"].as<string>();
+		setenv("GOJA_ROOT_FILENAME", ss.str().c_str(), 1);
+		Hits h;
+		h.Loop();
+	}
+	else if (vm.count("root-many")) {
+		stringstream ss;
+		ss << vm["root-many"].as<string>();
+		string substring = "";
+		vector<string> substrings;
+		while (getline(ss, substring, ',')) {
+			substrings.push_back(substring);
+		}
+		string basename = substrings[0];
+		int nr_of_files = stoi(substrings[1]);
 
-	Hits h;
-	h.Loop();
+		for (int i=1; i<=nr_of_files; i++) {
+			string root_filename = basename + to_string(i) + ".root";
+			setenv("GOJA_ROOT_FILENAME", root_filename.c_str(), 1);
+			Hits h;
+			h.Loop();
+		}
+	}
 
 	return 0;
 }

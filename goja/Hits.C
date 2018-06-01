d6f1c7ace9463a7d459fa0ff98ee1943fdb296e3
Paweł Kowalski <flukson@gmail.com>
2018-06-01 20:37:53 +0200

    Critical bugfix in goja in event creation section
    Old version of code caused that in the goja output were accidental
    coincidences with time difference bigger than time window.

--- goja/Hits.C
+++ goja/Hits.C
@@ -17,7 +17,8 @@ using namespace std;
 LoopResults Hits::Loop() {
 
 	double COMPTON_E_TH_0 = atof(getenv("GOJA_COMPTON_E_TH_0"));
-	double TIME_WINDOW = atof(getenv("GOJA_TIME_WINDOW"))/1e9;
+	double COMPTON_E_TH = atof(getenv("GOJA_COMPTON_E_TH"));
+	double TIME_WINDOW = atof(getenv("GOJA_TIME_WINDOW"))/1e9; // [TIME_WINDOW]=s
 	int EVENTS_SEPARATION_USING_TIME_WINDOW, EVENTS_SEPARATION_USING_IDS_OF_EVENTS;
 	int sep = int(atof(getenv("GOJA_SEP")));
 	if (sep==0) EVENTS_SEPARATION_USING_TIME_WINDOW=1;
@@ -34,6 +35,7 @@ LoopResults Hits::Loop() {
 	int last_eventID;
 	double last_time = 0.;
 	double start_time = 0.;
+	double start_time_window = 0.;
 
 	vector<Hit> event;
 
@@ -66,32 +68,38 @@ LoopResults Hits::Loop() {
 		hit.sourcePosZ = sourcePosZ;
 		hit.nPhantomCompton = nPhantomCompton;
 		hit.nCrystalCompton = nCrystalCompton;
-
 		string procName = string(processName);
+
 		bool hit_is_proper = hit.edep>COMPTON_E_TH_0 and						// deposited energy is bigger than the noise threshold
 		                     (procName=="Compton" or procName=="compt") and		// the photon is scattered using Compton scattering
 		                     nPhantomRayleigh==0 and nCrystalRayleigh==0 and	// the photon is not scattered using Rayleigh scattering
 		                     PDGEncoding==22;									// gamma photon
+
 		if (hit_is_proper) {						// if the current hit is proper (see above conditions)
 			if (event.size()==0) {
-				event.push_back(hit);
+				if (hit.edep>COMPTON_E_TH) { // start collecting data from hit with edep>E_TH and set start of time window to its time
+					event.push_back(hit);
+					start_time_window = time;
+				}
 			}
 			else {
 				bool hit_in_event =
 					(EVENTS_SEPARATION_USING_IDS_OF_EVENTS and last_eventID==eventID) or
-					(EVENTS_SEPARATION_USING_TIME_WINDOW and fabs(last_time-time)<TIME_WINDOW);
+					(EVENTS_SEPARATION_USING_TIME_WINDOW and fabs(start_time_window-time)<TIME_WINDOW);
 				if (hit_in_event) {					// if the current hit belongs to the current event
 					event.push_back(hit);			// then it is added to the current event
 				}
 				else {								// if the current hit does not belong to the current event
-					if (event.size()==0) hit.print_hit();
 					lr.multiplicities.push_back(event.size());
 					if (event.size()>=2) {			// if the number of hits in the current event is at least 2
 						EventAnalysis ea;
 						ea.analyze_event(event);	// then the current event is analyzed
 					}
 					event.clear();					// the current event is destroyed
-					event.push_back(hit);			// and the new event is created using the current hit
+					if (hit.edep>COMPTON_E_TH) {
+						event.push_back(hit);		// and the new event is created using the current hit (if edep>E_TH)
+						start_time_window = time;
+					}
 				}
 			}
 		}
